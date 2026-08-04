b66f451bb6920f0bebba259e9b9631363423740a
httphypixelnet <intenstudios369@gmail.com>
2026-08-03 23:00:21 -0700

    report error without postinstall

--- src/vale.ts
+++ src/vale.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { spawnSync } from "node:child_process";
-import { join } from "node:path";
-import { outputBinName } from "./shared";
-const result = spawnSync(
-  join(import.meta.dirname, "..", "native", outputBinName),
-  process.argv.slice(2),
-  {
+import { outputBin } from "./shared";
+import { existsSync } from "fs-extra";
+if (existsSync(outputBin)) {
+  const result = spawnSync(outputBin, process.argv.slice(2), {
     stdio: "inherit",
-  },
-);
-if (result.signal) {
-    console.error(`Process failed with signal: ${result.signal}`)
+  });
+  if (result.signal) {
+    console.error(`Process failed with signal: ${result.signal}`);
+  }
+  process.exit(result.status ?? 1);
 }
-process.exit(result.status ?? 1);
+console.error("Missing Vale binary. Did you run the postinstall?");
+process.exit(1);
